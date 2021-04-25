3b97cac1c721bdd1e0dedd91a7b3a772fbd4e991
DamianSmolczynski <d.smolczynski@o2.pl>
2021-04-25 21:24:02 +0200

    Nav bar

---
+++ menu-button-script.js
@@ -0,0 +1,20 @@
+let timesClicked = 0;
+
+document.querySelector(".menu-button").addEventListener("click", () => {
+    if(timesClicked == 0)
+    {
+        document.querySelector(".menu-wrapper").animate("menuUnwrap");
+        timesClicked++;
+    }
+    else
+    {
+        if(timesClicked % 2 != 0){
+            document.querySelector(".menu-wrapper").style.display ="none";
+            timesClicked++;
+        }
+        else{
+            document.querySelector(".menu-wrapper").style.display ="flex";
+            timesClicked++;
+        }
+    }
+});
